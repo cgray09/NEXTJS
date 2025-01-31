aec86a41547c072f5a35d5945ba870d15ddf2501
cgray09 <47266486+cgray09@users.noreply.github.com>
2025-01-31 11:42:42 -0800

    Add files via upload

---
+++ store-front/components/navbar/UserIcon.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { LuUser } from 'react-icons/lu';
+
+async function UserIcon() {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />
+    );
+  }
+
+  return <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />;
+}
+export default UserIcon;
